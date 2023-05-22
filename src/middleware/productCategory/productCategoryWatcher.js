e9b18983138267402116601d8a052b2a5e313e97
only1stepgithub <deepak2023formywork@gmail.com>
2023-05-23 02:43:02 +0530

    destry

--- src/middleware/productCategory/productCategoryWatcher.js
+++
@@ -1,7 +0,0 @@
-import { takeLatest } from "redux-saga/effects";
-import {productCategoryProcessor} from './productCategoryProcessor';
-import {INITIATE_PRODUCT_CATEGORY_PROCESSOR} from './productCategoryActionTypes';
-
-export const productCategoryWatcher=[
-    takeLatest(INITIATE_PRODUCT_CATEGORY_PROCESSOR, productCategoryProcessor)
-];
